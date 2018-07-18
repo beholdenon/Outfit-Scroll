159e07e564fa423223c5e3ab6a677e1aa822481f
Brian Holden <beholdenon@icloud.com>
2018-07-17 21:23:08 -0400

    Added masking div effect

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
 $(function() {
-	function loadJSON(file){ 
-		var jqxhr = $.getJSON( file, function(data) {
-		  console.log(data);
-		  parseData(data);
-		})
-		 .fail(function() {
-		   console.log( "error" );
-		   alert("fail");
-		 })
-	}
-
-	function parseData(data) {
-		console.log(data);
-		$.each( data, function( key, val ) {
-			console.log(key + " " + val.product_name)
-		});
-	}
-	loadJSON("./data/test.json");
+	var topOffset1 = $("li").eq(0).offset().top;
+	var topOffset2 = $("li").eq(4).offset().top + $("li").eq(4).height();
+	$( window ).scroll(function() {
+		var pos1 = $(window).scrollTop() - topOffset1;
+		var pos2 = $(window).scrollTop() - topOffset2;
+	  	if(pos1 < 0) {
+	  		$("li").eq(0).css("background-position", "center " +  (Math.abs(pos1) - 10) + "px");
+	  	}
+	  	else {
+	  		$("li").eq(0).css("background-position", "center 0px");
+	  	}
+	  	if(pos2 > -700) {
+	  		$("li").eq(4).css("background-position", "center " +  (-(pos2) - 701) + "px");
+	  	}
+	  	else {
+	  		$("li").eq(4).css("background-position", "center 0px");
+	  	}
+	});
 
 });
